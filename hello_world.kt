08534dbaf8f06d9fae4d9e0dd84fac4d1fc8c938
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-11-03 08:28:41 -0600

    .
    .

---
+++ hello_world.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    println("Hello World")
+    println("My first Kotlin program")
+
+    /*   --------------------- VARIABLES*/
+
+    var chris: String = "Christian Castillo" //String chris = "Christian Castillo" 
+    var apellido: String
+    apellido = " Covarrubias"
+
+    var salario: Int = 270
+    var mensual: Int = salario * 31
+    println(salario)
+    println(mensual)
+    println(chris+apellido)
+
+    val apples: Double = 6.5
+    val orange: Int = 5
+    val fruit: Int = apples - oranges
+    println(fuit)
+}
