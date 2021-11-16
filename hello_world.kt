754e6755e89b15c8754288fa710b177e658b12b5
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-11-15 19:01:09 -0600

    .

--- hello_world.kt
+++ hello_world.kt
@@ -3,19 +3,62 @@ fun main(args: Array<String>) {
     println("My first Kotlin program")
 
     /*   --------------------- VARIABLES*/
+    //VAR stands for VARIABLE
 
-    var chris: String = "Christian Castillo" //String chris = "Christian Castillo" 
+
+    var jim: String
+    var chris = "Christian Castillo" //String chris = "Christian Castillo" 
     var apellido: String
     apellido = " Covarrubias"
-
+    var lives: Int = 3    
     var salario: Int = 270
     var mensual: Int = salario * 31
-    println(salario)
-    println(mensual)
-    println(chris+apellido)
+    println("Salario: $salario")
+    println("Salario mensual: \$$mensual")
+    println("Nombre completo: ${chris+apellido}")
 
-    val apples: Double = 6.5
-    val orange: Int = 5
+    /* VAL stands for VALUE*/
+    //We declare variables as VAL when we don't expect them to change
+
+    jim = "James T Kirk"
+    val apples: Int = 6
+    val oranges: Int = 5
     val fruit: Int = apples - oranges
-    println(fuit)
+    val isGameOver: Boolean = (lives < 1)
+    println(fruit)
+
+    println("${apples/4}")
+    println()
+
+    val weeks: Int = 234
+    val years: Double = weeks/52.0
+
+    println("$weeks weeks in $years")
+    println("Jim is $jim")
+
+    println("-------------------CONDITIONALS--------------------")
+
+    println(isGameOver)
+    if (isGameOver){
+        println("Game over!!")
+    } else {
+        println("You're still alive!")
+    }
+
+    println("How old are you \\?")
+    //val age = readLine()!!.toInt()
+    val age = 21
+    println("You are $age years old!")
+
+    val message: String
+
+    if (age < 18) {
+        message = "You're to young to vote. \n\n"        
+    } else if (age == 100){
+        message = "Congratulations!"
+    } else {
+        message = "You can vote!"
+    }
+
+    println(message)
 }
